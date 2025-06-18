5b076879025cbfc502b06af8c91c908c4ca86317
TannerBebb <17tbebb@gmail.com>
2025-06-18 08:16:31 -0500

    I implemented the stringminutes variable and printed the response to confirm that it worked properly(June 18th 2025 08:16).

--- main.py
+++ main.py
@@ -5,4 +5,10 @@
     1.) Allow the user to input the time
     2.) Count down to 0
     3.) Tell the user that the time is up.
+
+    Im thinking that there is going to have to be some kind of for loop.
+    Will this timer countdown in minutes and seconds or just seconds?
 """
+
+stringMinutes = input("Please set a time to countdown(In minutes): ")
+print(stringMinutes)
